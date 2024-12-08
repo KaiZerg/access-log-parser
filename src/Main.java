077c446c6b4e5fb24050f584c4d1892d22033d85
kaizerg <gabitov.tatarin@gmail.com>
2024-12-08 10:12:10 +0300

    Added loops practice

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        Scanner scanner = new Scanner(System.in); // Создаем объект для чтения данных из консоли
+
+        int correctFileCount = 0; // Счетчик для хранения количества правильных путей к файлам
+
+        while (true) { // Бесконечный цикл
+            System.out.print("Введите путь к файлу: ");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+
+            boolean fileExists = file.exists();
+            boolean isFile = file.isFile();
+
+            if (!fileExists || !isFile) {
+                System.out.println("Файл не найден или указанный путь ведет к папке.");
+                continue;
+            }
+
+            correctFileCount++;
+
+            System.out.printf("Путь указан верно. Это файл номер %d.\n", correctFileCount);
+        }
     }
 }
